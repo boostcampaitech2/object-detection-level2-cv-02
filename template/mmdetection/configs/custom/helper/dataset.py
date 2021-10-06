ca9de624b8c0f69a77b6c764d3bfdca32aabb94b
hojihun5516 <hojihun5516@daum.net>
2021-10-06 06:14:56 +0000

    fix: Modify dataroot path
    resolve #23

--- template/mmdetection/configs/custom/helper/dataset.py
+++ template/mmdetection/configs/custom/helper/dataset.py
@@ -12,7 +12,7 @@ classes = (
     "Battery",
     "Clothing",
 )
-data_root = "../dataset/"
+data_root = "../../../dataset/"
 
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
